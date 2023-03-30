f2c2b56344db4a6d232bc84ab556b9802d3c06e7
Eduardo Villas Boas <eduardovillasboassantos@gmail.com>
2023-03-30 19:14:42 -0300

    feat: add thread pool for an queue consumer

--- src/main/kotlin/quarkus/consumer/QuoteConsumer.kt
+++ src/main/kotlin/quarkus/consumer/QuoteConsumer.kt
@@ -1,24 +1,18 @@
 package quarkus.consumer
 
-import io.reactivex.rxjava3.schedulers.Schedulers
 import io.smallrye.reactive.messaging.annotations.Blocking
-import io.vertx.core.VertxException
-import io.vertx.rxjava3.core.Vertx
 import org.eclipse.microprofile.reactive.messaging.*
 import org.slf4j.LoggerFactory
 import quarkus.Quote
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
-import javax.inject.Inject
-import javax.inject.Named
-import javax.ws.rs.Produces
 
 /**
  * A bean consuming data from the "quote-requests" RabbitMQ queue and giving out a random quote.
  * The result is pushed to the "quotes" RabbitMQ exchange.
  */
 @ApplicationScoped
-class QuoteConsumer(private val vertx: io.vertx.core.Vertx) {
+class QuoteConsumer {
 
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
@@ -28,25 +22,24 @@ class QuoteConsumer(private val vertx: io.vertx.core.Vertx) {
     lateinit var quoteResultEmitter: Emitter<Quote>
 
     @Incoming("quote-request-ch-in")
-    @Blocking
+    @Blocking(
+        value = "quote-request-thread-pool",
+        ordered = false
+    )
     @Throws(
         InterruptedException::class
     )
     fun process(quoteRequest: String) {
         logger.info("Starting process [thread: ${threadIdentification()}, message: ${quoteRequest}]")
 
-        Vertx(vertx).rxExecuteBlocking {
-            logger.info("Heavy process start [thread: ${threadIdentification()}, message: ${quoteRequest}]")
-            Thread.sleep(1000)
-            logger.info("Heavy process finish [thread: ${threadIdentification()}, message: ${quoteRequest}]")
-            val value = random.nextInt(100)
+        logger.info("Heavy process start [thread: ${threadIdentification()}, message: ${quoteRequest}]")
+        Thread.sleep(1000)
+        logger.info("Heavy process finish [thread: ${threadIdentification()}, message: ${quoteRequest}]")
+        val value = random.nextInt(100)
 
-            logger.info("Finishing process [thread: ${threadIdentification()}, message: ${quoteRequest}]")
-            it.complete(value)
-        }.subscribeOn(Schedulers.computation())
-            .subscribe {
-            quoteResultEmitter.send(Quote(quoteRequest, it))
-        }
+        logger.info("Finishing process [thread: ${threadIdentification()}, message: ${quoteRequest}]")
+
+        quoteResultEmitter.send(Quote(quoteRequest, value))
     }
 
     private fun threadIdentification() =
--- src/main/kotlin/quarkus/producer/QuotesProducer.kt
+++ src/main/kotlin/quarkus/producer/QuotesProducer.kt
@@ -14,6 +14,7 @@ import javax.ws.rs.core.MediaType
 
 
 @Path("/quotes")
+
 class QuotesProducer {
 
     @Channel("quote-request-ch-out")
