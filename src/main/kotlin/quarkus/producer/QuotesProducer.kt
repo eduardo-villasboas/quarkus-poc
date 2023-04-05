8e998e605b86bac99c2d892cdc8bb292fe1eb4b0
Eduardo Villas Boas <eduardovillasboassantos@gmail.com>
2023-04-05 02:02:46 -0300

    refactor: remove implementation dependency

--- src/main/kotlin/quarkus/producer/QuotesProducer.kt
+++ src/main/kotlin/quarkus/producer/QuotesProducer.kt
@@ -1,19 +1,19 @@
 package quarkus.producer
 
 import io.smallrye.mutiny.Multi
-import io.vertx.core.json.JsonObject
 import org.eclipse.microprofile.reactive.messaging.Channel
 import org.eclipse.microprofile.reactive.messaging.Emitter
 import org.jboss.resteasy.reactive.RestStreamElementType
-import org.jboss.resteasy.reactive.server.jaxrs.OutboundSseEventImpl
 import org.slf4j.LoggerFactory
 import java.util.*
 import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.sse.OutboundSseEvent
+import javax.ws.rs.sse.Sse
 
 
 @Path("/quotes")
@@ -23,7 +23,7 @@ class QuotesProducer {
     lateinit var quoteRequestEmitter: Emitter<String>
 
     @Channel("returned-quotes")
-    lateinit var quotes: Multi<JsonObject>
+    lateinit var quotes: Multi<Any>
 
     /**
      * Endpoint to generate a new quote request id and send it to "quote-requests" channel (which
@@ -44,11 +44,15 @@ class QuotesProducer {
 
     @GET
     @RestStreamElementType(MediaType.APPLICATION_JSON)
-    fun stream(): Multi<OutboundSseEvent> {
+    fun stream(@Context sse: Sse): Multi<OutboundSseEvent> {
         return quotes.map {
-            OutboundSseEventImpl.BuilderImpl().name("update").data(it).build()
+            sse.newEventBuilder()
+                .data(it)
+                .name("update")
+                .build()
         }
     }
 
-    private fun threadIdentification() = "Thread.id=${Thread.currentThread().id},Thread.name=${Thread.currentThread().name}"
+    private fun threadIdentification() =
+        "Thread.id=${Thread.currentThread().id},Thread.name=${Thread.currentThread().name}"
 }
